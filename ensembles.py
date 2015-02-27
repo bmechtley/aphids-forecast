a65f9f3ead1c4975446ed33f459d845c86f69691
plant <bmechtley@gmail.com>
2015-02-26 19:34:45 -0600

    make directories

--- ensembles.py
+++ ensembles.py
@@ -83,7 +83,7 @@ def interpolate_variable_worker(args):
 
     print args['id'], 'writing', args['npy']
 
-    os.makedirs(args['npy'])
+    os.makedirs(os.path.dirname(args['npy']))
     np.save(args['npy'], interpolated)
 
 
